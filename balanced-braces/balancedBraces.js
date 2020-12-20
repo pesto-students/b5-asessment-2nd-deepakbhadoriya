13e77f11c8fbc689a85ca96dcd5780395623a60c
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2020-12-20 17:18:55 +0530

    balanced braces done

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,8 +1,18 @@
-
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(string) {
+  let bracesArray = [];
+  var i = 0;
+  while (i < string.length) {
+    let currentChar = string.charAt(i);
+    if (currentChar === "(" || currentChar === "[" || currentChar === "{") bracesArray.push(currentChar);
+    if (currentChar === ")" || currentChar === "]" || currentChar === "}") {
+      let lastElement = bracesArray[bracesArray.length - 1];
+      (lastElement === "(" && currentChar === ")") || (lastElement === "[" && currentChar === "]") || (lastElement === "{" && currentChar === "}")
+        ? bracesArray.pop()
+        : bracesArray.push(currentChar);
+    }
+    i++;
+  }
+  return bracesArray.length === 0 ? true : false;
 }
 
-export {
-  balancedBraces,
-};
+module.exports = balancedBraces;
