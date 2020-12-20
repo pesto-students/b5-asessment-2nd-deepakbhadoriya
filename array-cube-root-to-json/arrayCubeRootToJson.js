e07ffb2fa2482413f44381318da7008f95ade3ed
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2020-12-20 16:08:42 +0530

    array cube root done

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,15 @@
-const arrayCubeRootToJson = arr => {};
+const arrayCubeRootToJson = (arr) => {
+  try {
+    let rootJson = {};
+    arr.map((number) => {
+      let cubeRoot = Math.cbrt(number);
+      if (cubeRoot === 0 || isNaN(number)) throw new Error();
+      rootJson[number] = cubeRoot;
+    });
+    return rootJson;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
-export { arrayCubeRootToJson };
+module.exports = arrayCubeRootToJson;
