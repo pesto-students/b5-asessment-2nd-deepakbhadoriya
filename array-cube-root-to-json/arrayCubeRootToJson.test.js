e0d08a246cecc02ec1263f08fd5f4d2ed7b37302
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2020-12-21 15:11:26 +0530

    array methods done

--- array-cube-root-to-json/arrayCubeRootToJson.test.js
+++ array-cube-root-to-json/arrayCubeRootToJson.test.js
@@ -4,13 +4,7 @@ describe("arrayCubeRootToJson", () => {
   it("find cube root of every element of array and return it as A json", () => {
     expect(arrayCubeRootToJson([27, 64, 125])).toMatchObject({ 27: 3, 64: 4, 125: 5 });
     expect(arrayCubeRootToJson(["27", "64", "125", 1])).toMatchObject({ 27: 3, 64: 4, 125: 5, 1: 1 });
-    expect(arrayCubeRootToJson([-1, 1, Infinity, 64, -64])).toMatchObject({
-      "-1": -1,
-      1: 1,
-      Infinity: Infinity,
-      64: 4,
-      "-64": -4,
-    });
+    expect(arrayCubeRootToJson([-1, 1, Infinity, 64, -64])).toMatchObject({ "-1": -1, 1: 1, Infinity: Infinity, 64: 4, "-64": -4 });
   });
 
   it("throw cases", () => {
--- array-utils/arrayUtils.js
+++ array-utils/arrayUtils.js
@@ -1,7 +1,85 @@
+Array.prototype.forEachArray = function (callBackFunction) {
+  try {
+    //Validating array input and callbackFunction
+    if (!Array.isArray(this)) {
+      throw new TypeError("Array.prototype.forEach called on null or undefined");
+    }
+    if (typeof callBackFunction !== "function") {
+      throw new TypeError(callBackFunction + " is not a function");
+    }
+    //Lopping thorough passed array and passing value to callback
+    let i = 0;
+    while (i < this.length) {
+      callBackFunction(this[i], i, this);
+      i++;
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
-export {
-  forEach,
-  map,
-  filter,
-  reduce,
-}
+Array.prototype.mapArray = function (callBackFunction) {
+  try {
+    //Validating array input and callbackFunction
+    if (!Array.isArray(this)) {
+      throw new TypeError("Array.prototype.forEach called on null or undefined");
+    }
+    if (typeof callBackFunction !== "function") {
+      throw new TypeError(callBackFunction + " is not a function");
+    }
+    //Lopping thorough passed array and passing value to callback
+    const tempArray = [];
+    let i = 0;
+    while (i < this.length) {
+      tempArray.push(callBackFunction(this[i], i, this));
+      i++;
+    }
+    return tempArray;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+Array.prototype.filterArray = function (callBackFunction) {
+  try {
+    //Validating array input and callbackFunction
+    if (!Array.isArray(this)) {
+      throw new TypeError("Array.prototype.forEach called on null or undefined");
+    }
+    if (typeof callBackFunction !== "function") {
+      throw new TypeError(callBackFunction + " is not a function");
+    }
+    //Lopping thorough passed array and passing value to callback
+    const tempArray = [];
+    let i = 0;
+    while (i < this.length) {
+      callBackFunction(this[i]) && tempArray.push(this[i]);
+      i++;
+    }
+    return tempArray;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+Array.prototype.reduceArray = function (callBackFunction, initialSum = 0) {
+  try {
+    //Validating array input and callbackFunction
+    if (!Array.isArray(this)) {
+      throw new TypeError("Array.prototype.forEach called on null or undefined");
+    }
+    if (typeof callBackFunction !== "function") {
+      throw new TypeError(callBackFunction + " is not a function");
+    }
+    //Lopping thorough passed array and passing value to callback
+    let sum = initialSum;
+    let i = 0;
+    while (i < this.length) {
+      sum = callBackFunction(sum, this[i], i, this);
+      i++;
+    }
+    return sum;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
